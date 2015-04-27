97777233057053a6cf70cb6b3c51ba7ff63cc83b
Pablo Holanda <phol4nda@hmail.com>
2015-04-27 19:39:22 -0300

    Enable Admin Interface

--- Gelileo_Django_Interface/sensors/admin.py
+++ Gelileo_Django_Interface/sensors/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from .models import LightSensor, TemperatureSensor
 
-admin.site.register(LightSensor)
-admin.site.register(TemperatureSensor)
+
+class SensorAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['value']}),
+        ('Date information', {'fields': ['reading_date'], 'classes': ['collapse']}),
+    ]
+    list_display = ('value', 'reading_date')
+    list_filter = ['reading_date']
+
+
+admin.site.register(LightSensor, SensorAdmin)
+admin.site.register(TemperatureSensor, SensorAdmin)
 
 # Register your models here.
